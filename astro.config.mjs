6a6dd4277e810b317e9357b1faeccdb0b8bb2afb
KamelElmaz <kamelgunners30@gmail.com>
2025-02-07 14:25:53 +0100

    Projects finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kamelelmaz.github.io',
+  base: 'MyPortfolio',
   i18n: {
     locales: ["en", "fr"],
     defaultLocale: "en"
